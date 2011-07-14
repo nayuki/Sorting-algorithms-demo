616d934567cfff98d37989238445f148e0dc5a80
Nayuki Minase <nayuki@eigenstate.org>
2011-07-14 16:49:44 +0000

    SortAlgorithm: Added class Javadoc comment.

--- src/nayuki/sortdemo/SortAlgorithm.java
+++ src/nayuki/sortdemo/SortAlgorithm.java
@@ -1,6 +1,9 @@
 package nayuki.sortdemo;
 
 
+/**
+ * A sorting algorithm, which is a sort of function object. Each subclass should have only a singleton instance, and the instance must be stateless.
+ */
 public abstract class SortAlgorithm {
 	
 	public abstract void sort(SortArray array);

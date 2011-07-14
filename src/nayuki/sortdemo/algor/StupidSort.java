b4def14889aa2ff070407247e03e24e6d27c78f9
Nayuki Minase <nayuki@eigenstate.org>
2011-07-14 04:30:01 +0000

    Clarified stupid sort algorithm.

--- src/nayuki/sortdemo/algor/StupidSort.java
+++ src/nayuki/sortdemo/algor/StupidSort.java
@@ -11,9 +11,12 @@ public class StupidSort extends SortAlgorithm {
 	
 	public void sort(SortArray array) {
 		array.setInactive(0, array.length());
-		for (int i = 1; i < array.length(); i++) {
-			if (array.compareAndSwap(i - 1, i))
+		int i = 0;
+		while (i < array.length() - 1) {
+			if (array.compareAndSwap(i, i + 1))
 				i = 0;
+			else
+				i++;
 		}
 		array.setDone(0, array.length());
 	}

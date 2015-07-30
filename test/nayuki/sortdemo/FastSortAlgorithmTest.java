05f249cb39a4a73c3696267abf3ee40c1b85f346
Nayuki Minase <nayuki@eigenstate.org>
2015-07-30 16:29:01 +0000

    SortAlgorithmTest, FastSortAlgorithmTest: Moved @Test and @Before annotations to the same line as method declaration.

--- test/nayuki/sortdemo/FastSortAlgorithmTest.java
+++ test/nayuki/sortdemo/FastSortAlgorithmTest.java
@@ -6,18 +6,15 @@ import org.junit.Test;
 // Contains test cases with larger arrays, appropriate for O(n log n) sort algorithms.
 public abstract class FastSortAlgorithmTest extends SortAlgorithmTest {
 	
-	@Test
-	public void testRandom1e3() {
+	@Test public void testRandom1e3() {
 		testRandom(1 * 1000);
 	}
 	
-	@Test
-	public void testRandom1e4() {
+	@Test public void testRandom1e4() {
 		testRandom(10 * 1000);
 	}
 	
-	@Test
-	public void testRandom1e5() {
+	@Test public void testRandom1e5() {
 		testRandom(100 * 1000);
 	}
 	
--- test/nayuki/sortdemo/SortAlgorithmTest.java
+++ test/nayuki/sortdemo/SortAlgorithmTest.java
@@ -13,31 +13,26 @@ public abstract class SortAlgorithmTest {
 	// Actual algorithm under test
 	protected SortAlgorithm algo;
 	
-	@Before
-	public void setUp() {
+	@Before public void setUp() {
 		algo = getInstance();
 	}
 	
 	
 	/* Test cases */
 	
-	@Test
-	public void testRandom10() {
+	@Test public void testRandom10() {
 		testRandom(10);
 	}
 	
-	@Test
-	public void testRandom30() {
+	@Test public void testRandom30() {
 		testRandom(30);
 	}
 	
-	@Test
-	public void testRandom100() {
+	@Test public void testRandom100() {
 		testRandom(100);
 	}
 	
-	@Test
-	public void testRandomSizes() {
+	@Test public void testRandomSizes() {
 		for (int i = 0; i < 100; i++)
 			testRandom(Utils.random.nextInt(100) + 1);
 	}
@@ -50,16 +45,14 @@ public abstract class SortAlgorithmTest {
 	}
 	
 	
-	@Test
-	public void testForward100() {
+	@Test public void testForward100() {
 		TestSortArray arr = new TestSortArray(100);
 		algo.sort(arr);
 		arr.assertSorted();
 	}
 	
 	
-	@Test
-	public void testReverse100() {
+	@Test public void testReverse100() {
 		TestSortArray arr = new TestSortArray(100);
 		arr.reverse();
 		algo.sort(arr);

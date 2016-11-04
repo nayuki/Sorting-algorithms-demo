7c4cc55fa908942aa13e423d237c90f4dfe63351
Project Nayuki <me@nayuki.io>
2016-11-04 05:07:17 +0000

    Added a new simple command line demo main program.

---
+++ src/io/nayuki/sortalgodemo/algo/simple/SimpleDemo.java
@@ -0,0 +1,84 @@
+/* 
+ * Sorting algorithms demo (Java)
+ * 
+ * Copyright (c) Project Nayuki
+ * https://www.nayuki.io/page/sorting-algorithms-demo-java
+ * 
+ * (MIT License)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
+
+package io.nayuki.sortalgodemo.algo.simple;
+
+import io.nayuki.sortalgodemo.algo.*;
+import io.nayuki.sortalgodemo.core.SortAlgorithm;
+
+
+public final class SimpleDemo {
+	
+	/*---- Default configuration ----*/
+	
+	private static final SortAlgorithm[] ALGORITHMS = {
+		HeapSort.INSTANCE,
+		QuickSortDoubleEnded.INSTANCE,
+		QuickSortSliding.INSTANCE,
+		ShellSort.INSTANCE,
+		BubbleSort.INSTANCE,
+		CocktailSort.INSTANCE,
+		SelectionSort.INSTANCE,
+		GnomeSort.INSTANCE,
+		InsertionSort.INSTANCE,
+		InsertionSortBinarySearch.INSTANCE,
+		PancakeSort.INSTANCE,
+		QuasiPancakeSort.INSTANCE,
+		StoogeSort.INSTANCE,
+		StupidSort.INSTANCE,
+		BozoSort.INSTANCE,
+		SlowSort.INSTANCE,
+	};
+	
+	private static final int DEFAULT_SIZE = 300;
+	
+	
+	
+	/*---- Main program ----*/
+	
+	public static void main(String[] args) {
+		// Handle command line arguments
+		int size;
+		if (args.length == 0)
+			size = DEFAULT_SIZE;
+		else if (args.length == 1)
+			size = Integer.parseInt(args[0]);
+		else {
+			System.err.println("Usage: java nayuki.sortalgodemo.algo.simple.SimpleDemo [ArraySize]");
+			System.exit(1);
+			return;
+		}
+		if (size <= 0)
+			throw new IllegalArgumentException("Array size must be positive");
+		
+		// Sort with each algorithm, and print statistics
+		SimpleSortArray referenceArray = new SimpleSortArray(size);
+		for (SortAlgorithm algo : ALGORITHMS) {
+			SimpleSortArray array = referenceArray.clone();
+			algo.sort(array);
+			System.err.printf("%s:  %d comparisons, %d swaps%n", algo.getName(), array.comparisonCount, array.swapCount);
+		}
+	}
+	
+}
---
+++ src/io/nayuki/sortalgodemo/algo/simple/SimpleSortArray.java
@@ -0,0 +1,83 @@
+/* 
+ * Sorting algorithms demo (Java)
+ * 
+ * Copyright (c) Project Nayuki
+ * https://www.nayuki.io/page/sorting-algorithms-demo-java
+ * 
+ * (MIT License)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
+
+package io.nayuki.sortalgodemo.algo.simple;
+
+import io.nayuki.sortalgodemo.core.AbstractSortArray;
+
+
+final class SimpleSortArray extends AbstractSortArray implements Cloneable {
+	
+	/*---- Fields ---*/
+	
+	public long comparisonCount;
+	public long swapCount;
+	
+	
+	
+	/*---- Constructors ----*/
+	
+	public SimpleSortArray(int size) {
+		super(size);
+		comparisonCount = 0;
+		swapCount = 0;
+		shuffle();
+	}
+	
+	
+	
+	/*---- Methods ----*/
+	
+	public int compare(int i, int j) {
+		comparisonCount++;
+		return super.compare(i, j);
+	}
+	
+	
+	public void swap(int i, int j) {
+		swapCount++;
+		super.swap(i, j);
+	}
+	
+	
+	public SimpleSortArray clone() {
+		try {
+			SimpleSortArray result = (SimpleSortArray)super.clone();
+			result.values = result.values.clone();
+			return result;
+		} catch (CloneNotSupportedException e) {
+			throw new AssertionError(e);
+		}
+	}
+	
+	
+	public void setActive(int index) {}
+	public void setInactive(int index) {}
+	public void setDone(int index) {}
+	
+	public void setActive(int start, int end) {}
+	public void setInactive(int start, int end) {}
+	public void setDone(int start, int end) {}
+	
+}
